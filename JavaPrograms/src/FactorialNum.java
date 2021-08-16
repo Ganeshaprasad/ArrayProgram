9473ce5f4d5d2340bc485d79ef4538bf7936383b
Ganeshaprasad <ganesha.prasad1996@gmail.com>
2021-08-16 21:24:12 +0530

    Array Programs commit

---
+++ JavaPrograms/src/FactorialNum.java
@@ -0,0 +1,14 @@
+
+public class FactorialNum {
+	
+	
+public static void main(String[] args) {
+                 int fact=1;
+                 for(int num=5;num>0;num--)
+                 {
+                	 fact=fact*num;
+                	 
+                 }
+                 System.out.println(fact);
+}
+}
